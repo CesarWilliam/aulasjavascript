7a828c6cda13d9f436da518006b058133475c2e6
william.rocha@bliteti.com.br <william.rocha@bliteti.com.br>
2019-07-10 15:43:37 -0300

    ajax - api publica - multiplas reqs

--- ajax/index.js
+++ ajax/index.js
@@ -5,6 +5,12 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/index-xmlhttprequest.html"); 
 })
 
+app.get("/aula_json.json", function(req, res){ 
+    res.sendFile(__dirname + "/aula_json.json"); 
+})
+
+app.use('/', express.static(__dirname + '/'));
+
 app.listen(8080, function(){
     console.log("Servidor rodando em http://localhost:8080")
 });
